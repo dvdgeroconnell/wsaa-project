7dc418d022a694b8d1595ac2f725e704eef204c0
David O'Connell <dvdgeroconnell@gmail.com>
2024-04-28 18:47:20 +0100

    start to builld DAO with CRUD methods

---
+++ recorddao.py
@@ -0,0 +1,26 @@
+# recorddao.py
+#
+# DAO file to implement the CRUD methods
+#
+# Author: David O'Connell
+#
+
+# return all LP records in the database table
+def get_all():
+    return [{}]
+
+# return one book
+def find_by_id(id):
+    return {}
+
+# insert a record into the database
+def create(record):
+    return record
+
+# update a record in the database
+def update(id, record):
+    return record
+
+# deletes the record identified by the id passed
+def delete(id):
+    return True
